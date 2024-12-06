ce96a9c860f1e7855ab6f892cfec3cabe7c6203d
pedrincaneladefogo <pedro.silva.camargo@escola.pr.gov.br>
2024-12-06 19:43:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mitologia',
+    'Quem é Zeus na mitologia grega?',
+    'Zeus é o deus do céu e do trovão, e o rei dos deuses no Olimpo na mitologia grega.'
+);
+
+criaCartao(
+    'Ecologia',
+    'O que é efeito estufa?',
+    'O efeito estufa é o aquecimento da atmosfera terrestre causado pela retenção de calor pelos gases estufa.'
+);
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Quixote"?',
+    'Miguel de Cervantes escreveu "Dom Quixote".'
+);
+
+criaCartao(
+    'Química',
+    'O que é o pH?',
+    'O pH é uma medida que indica a acidez ou basicidade de uma solução.'
+    );
